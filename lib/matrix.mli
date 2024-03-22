bf26b2eb6c6a126a51b4184dc7e64f959d3656ea
Serena <syz8@cornell.edu>
2024-03-22 18:18:00 -0400

    added matrix.mli matrix.ml

---
+++ lib/matrix.mli
@@ -0,0 +1,5 @@
+val add : float array array -> float array array -> float array array
+val subtract : float array array -> float array array -> float array array
+val transpose : float array array -> float array array
+val multiply : float array array -> float array array -> float array array
+val to_string : float array array -> string
