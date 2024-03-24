b35a6ac0c43fb5507f6cc42fdf76a3f0fa636a97
Emily <eyl48@cornell.edu>
2024-03-24 16:30:49 -0400

    Add documentation for basicops
    - Wrote specs for add, subtract, multiply, and divide functions

--- lib/basicops.ml
+++ lib/basicops.ml
@@ -1,4 +1,4 @@
 let add x y = x +. y
 let subtract x y = x -. y
 let multiply x y = x *. y
-let divide x y = if y <> 0. then x /. y else failwith "Arithmetic Error"
+let divide x y = x /. y
--- lib/basicops.mli
+++ lib/basicops.mli
@@ -1,4 +1,11 @@
 val add : float -> float -> float
+(** [add a b] adds floats [a] and [b] together. *)
+
 val subtract : float -> float -> float
+(** [subtract a b] subtract float [a] by float [b]. *)
+
 val multiply : float -> float -> float
+(** [multiply a b] multiplies floats [a] and [b] together. *)
+
 val divide : float -> float -> float
+(** [divide a b] divides float [a] by float [b]. Requires that [b] is not 0. *)
