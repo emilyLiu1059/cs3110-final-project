8c64d8d8528934dd55bde9a2a8fc3d068ca33125
Camille Yap <cy474@cornell.edu>
2024-03-25 14:49:01 -0400

    Modified Trig.mli file

--- lib/trig.mli
+++ lib/trig.mli
@@ -1,6 +1,8 @@
 val sin : int -> float
-(**[sin x] finds the sine of [x]*)
+(** [sin x] finds the sine of [x]*)
+
 val cos : int -> float
-(**[cos x] finds the sine of [x]*)
+(** [cos x] finds the sine of [x]*)
+
 val tan : int -> float
-(**[tan x] finds the sine of [x]*)
+(** [tan x] finds the sine of [x]*)
