d2dd163faa174849337eabee27bfdb26fb04a696
Camille Yap <cy474@cornell.edu>
2024-03-26 10:59:05 -0400

    Made some alterations in trig.ml file

--- lib/trig.ml
+++ lib/trig.ml
@@ -1,18 +1,18 @@
 let deg_to_rad x = x *. (Float.pi /. 180.0)
 
-let rec factorial x acc = if x = 0 then acc else factorial (x - 1) (acc *. x)
+let rec factorial x acc = if x = 0.0 then acc else factorial (x -. 1.0) (acc *. x)
 
 let rec sin_helper i acc value = 
-  if i = 10 then acc 
-  else let taylor = (((-1.0) ** float_of_int i) * (value ** (2.0 * i + 1))) /. float_of_int (factorial (2 * i + 1) 1) in 
-  sin_helper (i + 1) (acc +. taylor)
+  if i = 10.0 then acc 
+  else let taylor = (((-1.0) ** float_of_int i) * (value **. (2.0 *. i +. 1.0))) /. float_of_int (factorial (2.0 *. i +. 1.0) 1) in 
+  sin_helper (i +. 1.0) (acc +. taylor)
 
 let sin x = let x = deg_to_rad x in 
   sin_helper 0 0.0 x
 
 let cos_helper i acc value = 
-  if i = 10 then acc 
-  else let taylor = (((-1.0) ** float_of_int i) * (value ** (2.0 * i))) /. float_of_int (factorial (2 * i) 1) in 
+  if i = 10.0 then acc 
+  else let taylor = (((-1.0) **. float_of_int i) * (value ** (2.0 *. i))) /. float_of_int (factorial (2 * i) 1) in 
   cos_helper (i + 1) (acc +. taylor)
 
 let cos x = let x = deg_to_rad x in 
