d4f1ffee3afda5e8ca1d5e91083a76da6865a6c5
Emily <eyl48@cornell.edu>
2024-03-26 15:17:55 -0400

    Add basicop function to main
    - Tested add, subtract, multiply, and divide basicop functions in main

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,6 @@
-let () = print_endline "Hello, World!"
+open Final_project
+
+let _ = print_endline (string_of_float (Basicops.add 1. 2.))
+let _ = print_endline (string_of_float (Basicops.subtract 5. 3.))
+let _ = print_endline (string_of_float (Basicops.multiply 3. 4.))
+let _ = print_endline (string_of_float (Basicops.divide 8. 2.))
