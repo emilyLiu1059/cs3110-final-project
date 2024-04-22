a4078087ccdfef4e6cc6eb8f0e9e2af524a0e112
Camille Yap <cy474@cornell.edu>
2024-04-22 14:12:09 -0400

    Trig
    - Added csc, sec, cot, arcsin, arccos, and arctan
    - Wrote specs for all these functions

--- lib/trig.ml
+++ lib/trig.ml
@@ -14,7 +14,7 @@ let rec sin_helper i acc value =
     sin_helper (i +. 1.0) (acc +. taylor) value
 
 let sin x =
-  let x = deg_to_rad (float_of_int x) in
+  let x = deg_to_rad x in
   sin_helper 0.0 0.0 x
 
 let rec cos_helper i acc value =
@@ -26,7 +26,39 @@ let rec cos_helper i acc value =
     cos_helper (i +. 1.0) (acc +. taylor) value
 
 let cos x =
-  let x = deg_to_rad (float_of_int x) in
+  let x = deg_to_rad x in
   cos_helper 0.0 0.0 x
 
 let tan x = sin x /. cos x
+let csc x = 1. /. sin x
+let sec x = 1. /. cos x
+let cot x = 1. /. tan x
+
+let arcsin x =
+  x
+  +. ((x ** 3.) /. 6.)
+  +. ((x ** 5.) *. 3. /. 40.)
+  +. ((x ** 7.) *. 5. /. 112.)
+  +. ((x ** 9.) *. 35. /. 1152.)
+
+let arccos x =
+  (Float.pi /. 2.)
+  -. (x
+     +. ((x ** 3.) /. 6.)
+     +. ((x ** 5.) *. 3. /. 40.)
+     +. ((x ** 7.) *. 5. /. 112.)
+     +. ((x ** 9.) *. 35. /. 1152.))
+
+let arctan x =
+  let max_iterations = 20 in
+  let rec aux n acc =
+    if n >= max_iterations then acc
+    else
+      let term =
+        (-1.0 ** float_of_int n)
+        *. (x ** ((2.0 *. float_of_int n) +. 1.0))
+        /. ((2.0 *. float_of_int n) +. 1.0)
+      in
+      aux (n + 1) (acc +. term)
+  in
+  aux 0 0.0
--- lib/trig.mli
+++ lib/trig.mli
@@ -1,8 +1,26 @@
-val sin : int -> float
+val sin : float -> float
 (** [sin x] finds the sine of [x]*)
 
-val cos : int -> float
+val cos : float -> float
 (** [cos x] finds the cosine of [x]*)
 
-val tan : int -> float
+val tan : float -> float
 (** [tan x] finds the tangent of [x]*)
+
+val csc : float -> float
+(** [csc x] finds the cosecant of [x]*)
+
+val sec : float -> float
+(** [sec x] finds the secant of [x]*)
+
+val cot : float -> float
+(** [cot x] finds the cotangent of [x]*)
+
+val arcsin : float -> float
+(** [arcsin x] finds the arcsine/sine inverse of [x]*)
+
+val arccos : float -> float
+(** [arccos x] finds the arccosine/cosine inverse of [x]*)
+
+val arctan : float -> float
+(** [arctan x] finds the arctangent/tangent inverse of [x]*)
